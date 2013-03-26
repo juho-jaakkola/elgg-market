3683dbe3b668a374785033f519cd9efd7e40dbd1
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-03-26 22:20:28 +0200

    Notifies user when someone buys their item.

--- actions/market/buy.php
+++ actions/market/buy.php
@@ -10,17 +10,36 @@ if (elgg_instanceof($item, 'object', 'market_item')) {
 		forward(REFERER);
 	} else {
 		$user_guid = elgg_get_logged_in_user_guid();
-		
+
 		// "$user_guid is a buyer of $item"
-		$item->addRelationship($user_guid, 'buyer');
-		$item->status = 'sold';
-		$item->save;
-		
-		// TODO Add to river?
-		
-		// TODO Notify item owner
-		
-		system_message(elgg_echo('market:buy:success'));
+		if ($item->addRelationship($user_guid, 'buyer')) {
+			$item->status = 'sold';
+
+			if ($item->save()) {
+				$owner = $item->getOwnerEntity();
+
+				$subject = elgg_echo('market:item_bought:subject');
+				$message = elgg_echo('market:item_bought:message', array(
+					$owner->getName(),
+					$user->getName(),
+					$item->title,
+					$item->getURL()
+				));
+
+				notify_user(
+					$owner->getGUID(),
+					$user_guid,
+					$subject,
+					$message
+				);
+
+				// TODO Add to river?
+
+				system_message(elgg_echo('market:purchase:success'));
+			} else {
+				register_error(elgg_echo('market:error:purchase_failed'));
+			}
+		}
 	}
 } else {
 	register_error('noaccess');
--- languages/fi.php
+++ languages/fi.php
@@ -9,10 +9,21 @@ $finnish = array(
 	'market:buy' => 'Osta tämä kohde',
 	'market:buy:confirm' => 'Haluatko varmasti ostaa tämän kohteen?',
 	"market:status:sold" => 'Myyty',
-	
+
 	// Messages
 	'market:error:already_sold' => 'Tämä kohde on valitettavasti jo myyty!',
-	'market:buy:success' => 'Olet ostanut tämän kohteen',
+	'market:error:purchase_failed' => 'Ostaminen epäonnistui! Yritä uudelleen.',
+	'market:purchase:success' => 'Olet ostanut tämän kohteen',
+
+	// Notifications
+	'market:item_bought:subject' => 'Myyntiin laittamasi kohde on ostettu',
+	'market:item_bought:message' => 'Hei %s
+
+%s on ostanut myyntiin laittamasi kohteen "%s".
+
+Siirry kohteeseen tästä:
+%s	
+',
 );
 
 add_translation('fi', $finnish);
